d60027cb0fc9f13e3c816c295f3b44eb877d26a5
Dylan Elliott <dylan.elliott@gmail.com>
2019-06-01 17:42:25 +1000

    Add Chameleon and set global tint

--- Nudge/AppDelegate.swift
+++ Nudge/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Chameleon
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,16 +18,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        let tintColour: UIColor = .flatSkyBlue
+        let bgColour: UIColor = .white
+        
+        window!.tintColor = tintColour
+        
+        let switchAppearance = UISwitch.appearance()
+        switchAppearance.onTintColor = tintColour
+        
         let navBarAppearance = UINavigationBar.appearance()
-        navBarAppearance.barTintColor = .white
+        navBarAppearance.barTintColor = bgColour
         navBarAppearance.shadowImage = UIImage()
         
         let tabBarAppearance = UITabBar.appearance()
-        tabBarAppearance.backgroundColor = .white
+        tabBarAppearance.backgroundColor = bgColour
         tabBarAppearance.backgroundImage = UIImage()
         tabBarAppearance.shadowImage = UIImage()
         
-        UITableView.appearance().backgroundColor = .white
+        UITableView.appearance().backgroundColor = bgColour
         
         return true
     }
